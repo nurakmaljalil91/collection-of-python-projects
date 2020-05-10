de9671696e1417494b752448c8701bb5a26ae22d
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-05-10 23:04:33 +0800

    add select rect

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ done = ['update facebook']
 text = font.render(todo[0], True, RED)
 
 
+def select_rect():
+    pass
+
+
 def draw_grid(window, scale, color):
     for x in range(0, WIDTH, int(TILESIZE * scale)):
         pygame.draw.line(window, color, (x, 0), (x, HEIGHT))
@@ -42,7 +46,8 @@ def draw_rect(window, x, y, width, height, color):
     pygame.draw.line(window, color, (x, y + height), (x + height, y + height))
     pygame.draw.line(window, color, (x + width, y), (x + height, y + height))
 
-
+x = 30
+y = 45
 while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -50,13 +55,25 @@ while is_running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 is_running = False
+            if event.key == pygame.K_DOWN or event.key == pygame.K_j:
+                y += 30
+            if event.key == pygame.K_UP or event.key == pygame.K_k:
+                y -= 30
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_l:
+                x = 660
+                y = 45
+            if event.key == pygame.K_LEFT or event.key == pygame.K_h:
+                x = 30
+                y = 45
         print(pygame.mouse.get_pos())
-    # draw_grid(window, 1, DARKGRAY)
+    window.fill(BLACK)
+    draw_grid(window, 1, DARKGRAY)
     # window.blit(rect,(30,30))
     # pygame.draw.rect(window, RED, (30,30, 720,480))
-    draw_rect(window, 30, 45, 600, 600, RED)
-    draw_rect(window, 660, 45, 600, 600, BLUE)
-    window.blit(text, (48,63))
+    draw_rect(window, 30, 45, 600, 600, YELLOW)
+    draw_rect(window, 660, 45, 600, 600, YELLOW)
+    pygame.draw.rect(window, YELLOW, (x, y, 600, 30))
+    window.blit(text, (48, 63))
 
     pygame.display.flip()
     pygame.display.update()
