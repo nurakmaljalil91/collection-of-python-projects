e0f1c41777d6265db602abce228f57f138c43aba
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-05-10 16:24:07 +0800

    create window

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import pygame
 
+(width , height) = (300, 300)
+
+window = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption('Todo-py-qt')
+
+pygame.display.flip()
+
+is_running = True
+
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
