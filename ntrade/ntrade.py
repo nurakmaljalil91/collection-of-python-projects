4eeb9cfcc075df35e14d4c4ed6c4ad689c766e47
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-07-25 23:09:34 +0800

    adding window in ntrade

--- ntrade/ntrade.py
+++ ntrade/ntrade.py
@@ -1,2 +1,14 @@
 from tkinter import *
 
+window = Tk()
+
+window.title('Ntrade')
+
+window.geometry('800x640')
+
+lbl = Label(window, text='Hello World')
+
+lbl.grid(column=0, row=0)
+
+window.mainloop()
+
