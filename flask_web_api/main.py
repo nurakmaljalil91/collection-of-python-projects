24bd3f5bf2c7a7917405f4e050d346134c92718d
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-10-24 19:38:00 +0800

    add web api with flask

---
+++ flask_web_api/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+videos = {}
+
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        return
+
+
+api.add_resource(Video, '/video/<int:video_id>')
+if __name__ == '__main__':
+    app.run(debug=True)
